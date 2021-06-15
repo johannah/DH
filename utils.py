7314d992164c51429efb03935694d6cdd8a431c8
Sahand <sahand.rezaei.sh@gmail.com>
2021-06-15 08:05:51 -0400

    Fix a stupid mistake...

--- utils.py
+++ utils.py
@@ -418,7 +418,7 @@ def get_replay_state_dict(replay_buffer, use_states=[]):
 
 
 def parse_slurm_task_id(cfg, slurm_task_id, n_seeds=5):
-    cfg['seed'] += (slurm_task_id % n_seeds) * 3000
+    cfg['experiment']['seed'] += (slurm_task_id % n_seeds) * 3000
     if slurm_task_id // n_seeds == 0:
         cfg['robot']['env_name'] = 'Door'
     elif slurm_task_id // n_seeds == 1:
